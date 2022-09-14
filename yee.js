754f07303f40959e12dd33dca8967f58fab345d5
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-09-14 16:56:03 +0300

    add yee.js

---
+++ yee.js
@@ -0,0 +1,14 @@
+console.log("yee")
+
+const func = () => {
+	let var = 5
+	return var
+}
+
+a = 1
+
+b = 2
+
+string = "this is a hunk to save"
+
+hunk = "in the same hunk to save"
--- yee.py
+++ yee.py
@@ -3,3 +3,7 @@ print("yee")
 a = 1
 
 b = 2
+
+string = "this is a hunk to save"
+
+hunk = "another hunk to save"
